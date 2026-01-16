d2af6495e53cb7ad60df9b364353c47620a9bc0c
Angel Almanza <almanza.angel245@gmail.com>
2026-01-16 15:41:47 -0700

    feat: add share button to RestaurantCard for sharing restaurant links on Facebook

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 import type { Restaurant } from "@/interfaces/restaurants"
-import { Globe, Mail, MapPin, Phone, Star } from "lucide-react"
+import { Globe, Mail, MapPin, Phone, Share2, Star } from "lucide-react"
 import { Badge } from "./ui/badge"
+import { Button } from "./ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
 
 type Props = {
@@ -8,15 +9,27 @@ type Props = {
 }
 
 export const RestaurantCard = ({ restaurant }: Props) => {
+  const handleClickShare = () => {
+    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(restaurant.contact.site)}`
+    window.open(shareUrl, "_blank", "noopener,noreferrer")
+  }
+
   return (
     <Card>
       <CardHeader>
-        <div className="size-full relative hidden lg:block">
-          <img src="/images/placeholder.png" className="w-full h-32 rounded object-cover" />
-          <Badge variant="secondary" className="absolute top-2 right-2 bg-yellow-300 text-gray-800 text-xl">
-            {restaurant.rating}
-            <Star className="ml-1" />
-          </Badge>
+        <div className="size-full relative">
+          <img src="/images/placeholder.png" className="w-full h-32 rounded object-cover hidden lg:block" />
+          <div className="absolute -top-8 -right-8 lg:top-2 lg:right-2 flex items-center gap-x-4">
+            <Badge variant="secondary" className="bg-yellow-300 text-gray-800 text-xl">
+              {restaurant.rating.toFixed(1)}
+              <Star className="ml-1" />
+            </Badge>
+            <Badge asChild>
+              <Button variant="link" className="bg-blue-600 text-white text-xl cursor-pointer" onClick={handleClickShare}>
+                <Share2 />
+              </Button>
+            </Badge>
+          </div>
         </div>
         <CardTitle>{restaurant.name}</CardTitle>
       </CardHeader>
