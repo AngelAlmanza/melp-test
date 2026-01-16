153fe3f5ec08d3a6213a45b459571fd5677fc3ce
Angel Almanza <almanza.angel245@gmail.com>
2026-01-16 16:55:58 -0700

    fix: add comment for Leaflet marker icon issue and format options

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.tsx'
 
 // This section is to fix missing marker icons in Leaflet
+// https://github.com/PaulLeCam/react-leaflet/issues/808#issuecomment-934885702
 import L from 'leaflet';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
@@ -13,9 +14,9 @@ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 delete (L.Icon.Default.prototype as any)._getIconUrl
 L.Icon.Default.mergeOptions({
-    iconUrl: markerIcon,
-    iconRetinaUrl: markerIcon2x,
-    shadowUrl: markerShadow,
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
 })
 
 createRoot(document.getElementById('root')!).render(
